c1cab728aae3036223f82b6e19b5c6ab3c842666
saads2018 <71245465+SaudSultan9@users.noreply.github.com>
2021-11-02 00:21:10 +0800

    Modified Products Page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,13 @@
 <?php include 'header.php'?>
-<?php ?>
+
 <!doctype html>
+
+<style>
+	img{
+		max-height:350px;
+	}
+</style>
+
 <html class="no-js" lang="en">
 
     <head>
@@ -14,8 +21,10 @@
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
         
         <!-- title of site -->
-        <title>Cacti Products</title>
+        <title>Cacti Succulent KCH Products</title>
 
+        <!-- For favicon png -->
+		<link rel="shortcut icon" type="image/icon" href="logo/favicon.png"/>
        
         <!--font-awesome.min.css-->
         <link rel="stylesheet" href="css/font-awesome.min.css">
@@ -42,30 +51,230 @@
         <!--responsive.css-->
         <link rel="stylesheet" href="css/responsive.css">
         
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		
-        <!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
 
     </head>
 	
 	<body>
-		<!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
 		
+	
+		<!--welcome-hero start -->
+		<header id="home" class="welcome-hero">
 
-		
-		<!--welcome-hero end -->
+			<div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+				<!--/.carousel-indicator -->
+				 <ol class="carousel-indicators">
+					<li data-target="#header-carousel" data-slide-to="0" class="active"><span class="small-circle"></span></li>
+					<li data-target="#header-carousel" data-slide-to="1"><span class="small-circle"></span></li>
+					<li data-target="#header-carousel" data-slide-to="2"><span class="small-circle"></span></li>
+				</ol><!-- /ol-->
+				<!--/.carousel-indicator -->
+
+				<!--/.carousel-inner -->
+				<div class="carousel-inner" role="listbox">
+					<!-- .item -->
+					<div class="item active">
+						<div class="single-slide-item slide1">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Cacti collection</h4>
+													<h2 style="color:black;">Echeveia BlackPrince</h2>
+													<p>
+													Echeveria 'Black Prince' is an evergreen succulent forming striking rosettes, nearly black leaves which surround a glowing green center. In the fall and winter, it sends up leafy stems topped with remarkable clusters of bright scarlet-red flowers. 
+													</p>
+													<div class="packages-price">
+														<p>
+															RM 399.00
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														add <span>to</span> cart
+													</button>
+													<button class="btn-cart welcome-add-cart welcome-more-info" onclick="window.location.href='#'">
+														more info
+													</button>
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+													<img src="images/collection/cacti1.jpg" style="max-height:500px">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+
+					</div><!-- /.item .active-->
+
+					<div class="item">
+						<div class="single-slide-item slide2">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Cacti Collection</h4>
+													<h2 style="color:black;">Sedum Burrito</h2>
+													<p>
+													Sedum morganianum, the donkey tail or burro's tail, is a species of flowering plant in the family Crassulaceae, native to southern Mexico and Honduras. It is a succulent perennial producing trailing stems up to 60 cm long.</p>
+													<div class="packages-price">
+														<p>
+															RM 199.00
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														add <span>to</span> cart
+													</button>
+													<button class="btn-cart welcome-add-cart welcome-more-info" onclick="window.location.href='#'">
+														more info
+													</button>
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+												<img src="images/collection/cacti2.jpg" style="max-height:500px">
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+
+					</div><!-- /.item .active-->
+
+					<div class="item">
+						<div class="single-slide-item slide3">
+							<div class="container">
+								<div class="welcome-hero-content">
+									<div class="row">
+										<div class="col-sm-7">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-txt">
+													<h4>Cacti Collection</h4>
+													<h2 style="color:black;">Kalanchoe Rhombipilosa</h2>
+													<p>
+													Kalanchoe rhombopilosa is a beautiful and unique succulent that looks like a precious stone or a silver butterfly. A big plus side of Kalanchoe rhombopilosa is that it doesn’t grow too large, therefore it doesn’t occupy a lot of space. </p>
+													<div class="packages-price">
+														<p>
+															RM 299.00
+														</p>
+													</div>
+													<button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+														<span class="lnr lnr-plus-circle"></span>
+														add <span>to</span> cart
+													</button>
+													<button class="btn-cart welcome-add-cart welcome-more-info" onclick="window.location.href='#'">
+														more info
+													</button>
+												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+										<div class="col-sm-5">
+											<div class="single-welcome-hero">
+												<div class="welcome-hero-img">
+												<img src="images/collection/cacti3.jpg" style="max-height:500px">												</div><!--/.welcome-hero-txt-->
+											</div><!--/.single-welcome-hero-->
+										</div><!--/.col-->
+									</div><!--/.row-->
+								</div><!--/.welcome-hero-content-->
+							</div><!-- /.container-->
+						</div><!-- /.single-slide-item-->
+						
+					</div><!-- /.item .active-->
+				</div><!-- /.carousel-inner-->
+
+			</div><!--/#header-carousel-->
+		</header><!--/.welcome-hero-->
+
+
+		<!--populer-products start -->
+		<section id="populer-products" class="populer-products">
+			<div class="container">
+				<div class="populer-products-content">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="single-populer-products">
+								<div class="single-populer-product-img mt40">
+									<img src="images/collection/pcacti.png" style="max-height:125px;">
+								</div>
+								<h2><a href="#">Pots</a></h2>
+								<div class="single-populer-products-para">
+									<p>Pots of the Highest Quality</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="single-populer-products">
+								<div class="single-inner-populer-products">
+									<div class="row">
+										<div class="col-md-4 col-sm-12">
+											<div class="single-inner-populer-product-img">
+											<img src="images/collection/pcacti2.png" style="max-height:800px; padding-top:35px;">
+											</div>
+										</div>
+										<div class="col-md-8 col-sm-12">
+											<div class="single-inner-populer-product-txt">
+												<h2>
+													<a href="#">
+														Plants <span>and</span> Succulents
+													</a>
+												</h2>
+												<p>
+													We sell to our customers plants we believe in. No error is made in preparation and no limitation is placed in execution.  
+												</p>
+												<div class="populer-products-price">
+													<h4>Sales Start from <span>RM 15.00</span></h4>
+												</div>
+												<button class="btn-cart welcome-add-cart populer-products-btn" onclick="window.location.href='#'">
+													discover more
+												</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="single-populer-products">
+								<div class="single-populer-products">
+									<div class="single-populer-product-img">
+									<img src="images/collection/pcacti3.png" style="max-height:150px; padding-top:70px" >
+									</div>
+									<h2><a href="#">Soils</a></h2>
+									<div class="single-populer-products-para">
+										<p>Soils of the Highest Grade </p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><!--/.container-->
+
+		</section><!--/.populer-products-->
+		<!--populer-products end-->
 
 		<!--new-arrivals start -->
 		<section id="new-arrivals" class="new-arrivals">
 			<div class="container">
 				<div class="section-header">
-					<h2><br><br>PRODUCTS</h2>
+					<br>
+					<br>
+					<h2>All Products</h2>
+
 				</div><!--/.section-header-->
 				<div class="new-arrivals-content">
 					<div class="row">
@@ -73,8 +282,19 @@
 							<div class="single-new-arrival">
 								<div class="single-new-arrival-bg">
 									<img src="images/collection/cacti1.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
 								</div>
-								<h4><a href="#">Echeveia 'BlackPrince'</a></h4>
+								<h4><a href="#">Echeveia BlackPrince</a></h4>
 								<p class="arrival-product-price">RM 65.00</p>
 							</div>
 						</div>
@@ -82,8 +302,19 @@
 							<div class="single-new-arrival">
 								<div class="single-new-arrival-bg">
 									<img src="images/collection/cacti2.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
 								</div>
-								<h4><a href="#">Kalanchoe Rhombipilosa </a></h4>
+								<h4><a href="#">Sedum Burrito</a></h4>
 								<p class="arrival-product-price">RM 80.00</p>
 							</div>
 						</div>
@@ -91,8 +322,19 @@
 							<div class="single-new-arrival">
 								<div class="single-new-arrival-bg">
 									<img src="images/collection/cacti3.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
 								</div>
-								<h4><a href="#">Sedum Burrito</a></h4>
+								<h4><a href="#">Ragnoria Plantus</a></h4>
 								<p class="arrival-product-price">RM 40.00</p>
 							</div>
 						</div>
@@ -100,37 +342,99 @@
 							<div class="single-new-arrival">
 								<div class="single-new-arrival-bg">
 									<img src="images/collection/cacti4.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
 								</div>
-								<h4><a href="#">Bryophyllum</a></h4>
+								<h4><a href="#">Robere Pansy</a></h4>
 								<p class="arrival-product-price">RM 100.00</p>
-							</div>
-						</div>
-						
-						</div>
-						<div class="col-md-3 col-sm-4">
-							<div class="single-new-arrival">
-								<div class="single-new-arrival-bg">
-									<img src="images/collection/cacti7.jpg" alt="new-arrivals images">
-								</div>
-								<h4><a href="#">Plantus Whiterus</a></h4>
-								<p class="arrival-product-price">RM 140.00</p>
-							</div>
-						</div>
-						<div class="col-md-3 col-sm-4">
-							<div class="single-new-arrival">
-								<div class="single-new-arrival-bg">
-									<img src="images/collection/cacti6.jpg" alt="new-arrivals images">
-								</div>
-								<h4><a href="#">Crassula Lanuginose</a></h4>
-								<p class="arrival-product-price">RM 90.00</p>
 							</div>
 						</div>
 						<div class="col-md-3 col-sm-4">
 							<div class="single-new-arrival">
 								<div class="single-new-arrival-bg">
 									<img src="images/collection/cacti5.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
 								</div>
-								<h4><a href="#">Echeveria Pansy</a></h4>
+								<h4><a href="#">Kalanchoe Rhombipilosa</a></h4>
+								<p class="arrival-product-price">RM 120.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="images/collection/cacti6.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Cacti Ubulotus</a></h4>
+								<p class="arrival-product-price">RM 140.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="images/collection/cacti7.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Lotus Pora</a></h4>
+								<p class="arrival-product-price">RM 90.00</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-4">
+							<div class="single-new-arrival">
+								<div class="single-new-arrival-bg">
+									<img src="images/collection/cacti8.jpg" alt="new-arrivals images">
+									<div class="single-new-arrival-bg-overlay"></div>
+									<div class="new-arrival-cart">
+										<p>
+											<span class="lnr lnr-cart"></span>
+											<a href="#">add <span>to </span> cart</a>
+										</p>
+										<p class="arrival-review pull-right">
+											<span class="lnr lnr-heart"></span>
+											<span class="lnr lnr-frame-expand"></span>
+										</p>
+									</div>
+								</div>
+								<h4><a href="#">Plantus Whiterus</a></h4>
 								<p class="arrival-product-price">RM 140.00</p>
 							</div>
 						</div>
@@ -140,6 +444,8 @@
 		
 		</section><!--/.new-arrivals-->
 		<!--new-arrivals end -->
+	
+		<!-- clients end -->
 		
 		<!-- Include all js compiled plugins (below), or include individual files as needed -->
 
@@ -163,11 +469,14 @@
         
         <!--Custom JS-->
         <script src="js/custom.js"></script>
-        
-		<!--footer start -->
-		<?php include 'footer.php'?>
-		<!--footer end -->
 
-    </body>
+
+	</body>
+
+	<?php include 'footer.php'?>
+
+
+        
+    
 	
 </html>
