52a4e463fd3b237916f8ebbba3a957546aff13b6
AaronRJ99 <80440300+AaronRJ99@users.noreply.github.com>
2021-12-01 09:46:59 +0800

    Create HomepageTest.php

---
+++ tests/Unit/HomepageTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use Tests\TestCase;
+
+class HomepageTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_routeToHomepage()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200); 
+    }
+
+    public function test_routeToProductPage()
+    {
+        $response = $this->get('/product');
+        $response->assertStatus(200);
+    } 
+
+    public function test_routeToCartPage()
+    {
+        $response = $this->get('/cart');
+        $response->assertStatus(200);
+    }
+
+    public function test_routeToRegister()
+    {
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+
+    public function test_IncreaseCartProducts(){
+        $response = $this->get('cart/increaseCartQuantity/{Product_ID}');
+        $response->assertStatus(200);   
+    }
+
+    public function test_DecreaseCartProducts(){
+        $response = $this->get('cart/decreaseCartQuantity/{Product_ID}');
+        $response->assertStatus(200);
+    }
+}
