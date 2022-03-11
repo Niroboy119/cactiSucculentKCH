8409a21c37c446d94d8315683a4838ecb71fe6f6
Nirodha <niroman119@gmail.com>
2022-03-11 23:49:54 +0800

    added tracking personal info of guest users when making order requests

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
     
     public function store(Request $request){
 
@@ -21,7 +23,24 @@ class OrderController extends Controller
         }
 
         $order = new Order();
-        $order->user_Id = auth()->id();
+
+        if (auth()->check()) { // if the order request is made by a already registered users
+
+            $order->user_Id = auth()->id();
+
+        }else { // make a new user of type 'guest' and store their personal information into the users table
+            // $new_user_record = new User();
+            // $new_user_record->user_type = 'guest';
+            // $new_user_record->name = $request->firstName.' '.$request->lastName;
+            // $new_user_record->email = $request->email;
+            // $new_user_record->cust_address = $request->address;
+            // $new_user_record->cust_phone_number = $request->phonenumber;
+            // $new_user_record->password = null;
+            $guest = DB::table('users')->insertGetId(['user_type' => 'guest', 'name' => ($request->firstName.' '.$request->lastName), 'email' => $request->email, 'cust_address' => $request->address, 'cust_phone_number' => $request->phonenumber, 'password' => 123456789]);
+
+            $order->user_Id = $guest;
+        }
+
         $order->grand_total = $order_total;
         $order->item_count = count((array) session('cart'));
         $order->delivery_type = $request->inlineRadioOptions;
@@ -33,7 +52,7 @@ class OrderController extends Controller
         return redirect('/homepage');
     }
 	
-	 public function updateS($id)
+	public function updateS($id)
     {
         Order::where('order_Id', $id)->update(array('status' => 'cancelled'));
         return redirect('/order');
